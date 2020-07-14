518bbc127c9bd873bce5422266b6d0d3711491c8
Hao Fu <haofu@ucdavis.edu>
2020-07-14 15:11:30 +0800

    Remove unnecessary "final"

--- src/main/java/patdroid/util/Pair.java
+++ src/main/java/patdroid/util/Pair.java
@@ -19,9 +19,9 @@
 
 package patdroid.util;
 
-public final class Pair<T1, T2> {
-    final public T1 first;
-    final public T2 second;
+public class Pair<T1, T2> {
+    public T1 first;
+    public T2 second;
 
     public Pair(T1 first, T2 second) {
         this.first = first;
